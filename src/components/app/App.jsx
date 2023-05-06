88b445368156ba27bb5a5dda9b6b7118203e3dc1
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-05-06 17:19:08 +0300

    feat: add lazy loading

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,25 +1,30 @@
+import { lazy, Suspense } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainPage from "../../pages/MainPage";
-import ComicsPage from "../../pages/ComicsPage";
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import SingleComicsPage from "../../pages/SingleComicsPage";
+import Spinner from "../spinner/Spinner";
+const MainPage = lazy(() => import("../../pages/MainPage"));
+const SingleComicsPage = lazy(() => import("../../pages/SingleComicsPage"));
+const ComicsPage = lazy(() => import("../../pages/ComicsPage"));
+const ErrorMessage = lazy(() => import("../ErrorMessage/ErrorMessage"));
+
 const App = () => {
   return (
     <Router>
       <div className='app'>
         <AppHeader />
         <main>
-          <Routes>
-            <Route exact path='/' element={<MainPage />} />
-            <Route exact path='/comics' element={<ComicsPage />} />
-            <Route
-              exact
-              path='/comics/:comicId'
-              element={<SingleComicsPage />}
-            />
-            <Route exact path='*' element={<ErrorMessage />} />
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route exact path='/' element={<MainPage />} />
+              <Route exact path='/comics' element={<ComicsPage />} />
+              <Route
+                exact
+                path='/comics/:comicId'
+                element={<SingleComicsPage />}
+              />
+              <Route exact path='*' element={<ErrorMessage />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </Router>
